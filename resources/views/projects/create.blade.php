a671d1ec95d65d81e8428af2fb84d06a36a87387
Unknown <bonoarts2000@gmail.com>
2019-01-06 10:41:25 +0100

    lesson 17

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>projects</title>
-</head>
-<body>
+@extends("layout")
+
+
+@section('content')
+
     <h1>Create New</h1>
     <form method="POST"action="/projects" class="form-group">
         {{ csrf_field() }}
@@ -20,5 +16,4 @@
             <button type="submit" class="btn btn-primary">submit</button>
         </div>
     </form>
-</body>
-</html>
+@endsection
